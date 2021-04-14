42a40658fd2928cfd78787c6666f503ec7f0a86c
Artem4242 <artem20vorobiev@gmail.com>
2021-04-14 19:07:54 +0300

    site

--- authorization/connect.php
+++ authorization/connect.php
@@ -2,7 +2,7 @@
 
     $connect = new PDO('mysql:host = localhost, dbname = artem', 'artem', '123123');
 
-    // ПРОВЕРКА ПОДКЛЮЧЕНИЯ К БАЗЕ ДАННЫХ
+    // ПРОВЕРКА ПОДКЛЮЧЕНИЯ К БАЗЕ ДАННЫХ 
     if(!$connect){
     echo 'Error';
     }
